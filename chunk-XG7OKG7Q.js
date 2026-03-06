7ddc15cab99b07397e7bcc237c735b0dd7788516
Luixma <luisma1393@gmail.com>
2026-03-06 14:00:39 +0100

    Updated prices & terms. Added Calendar feature

---
+++ chunk-XG7OKG7Q.js
@@ -0,0 +1,1 @@
+var n={production:!0,apiBaseUrl:"https://elise-unsectionalized-soothingly.ngrok-free.dev",branding:{logoUrl:"/logo.png",logoWhiteUrl:"/logo-white.png",iconUrl:"/icon.png"}};export{n as a};
